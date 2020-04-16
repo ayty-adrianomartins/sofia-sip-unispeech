403060a164d31080cc96ed02aff81ad3ae8ef0ea
Adriano Martins <adrianomartins@gmail.com>
2020-04-16 10:28:43 -0300

    Fix crash on unresolvable dns resolution error of outbound request.

--- libsofia-sip-ua/nta/nta.c
+++ libsofia-sip-ua/nta/nta.c
@@ -10754,6 +10754,8 @@ void outgoing_answer_a(sres_context_t *orq, sres_query_t *q,
 
   if (results)
     outgoing_query_results(orq, sq, results, found);
+  else if (!q)
+    outgoing_resolving_error(orq, SIPDNS_503_ERROR);
 }
 
 /** Store A/AAAA query results */
